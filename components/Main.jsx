1d46f0a41dc1f7202dd7b90247377c5f1702d008
luis-more <mmore3369@gmail.com>
2022-07-05 12:03:01 -0500

    despues de fallidos intentos por cambiar una imagen

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,40 +1,54 @@
-import React from "react";
-
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-import { AiOutlineMail } from "react-icons/ai";
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const Main = () => {
   return (
-    <div className=" p-4 w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto  p-2 flex justify-center items-center">
+    <div id='home' className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-          <p className="uppercase text-sm -tracking-widest text-gray-600">
-            VAMOS A CREAR ALGO INCREIBLE JUNTOS
+          <p className='uppercase text-sm tracking-widest text-gray-600'>
+            VAMOS A CONSTRUIR ALGO INCREIBLE JUNTOS
           </p>
-          <h1 className="py-4 text-gray-700 text-3xl sm:text-4xl">
-            Hi, Soy<spam className="text-[#5651e5]"> Luis More</spam>
+          <h1 className='py-4 text-gray-700'>
+            Hola soy <span className='text-[#5651e5]'> Luis More</span>
           </h1>
-          <h1 className="py-2 text-gray-700 text-3xl sm:text-4xl">Desarrollador web</h1>
-          <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
-            Soy un especialista en crear y desarrollar paginas web. Ocasionalmente diseños
-            ecepcioinales con experiencias digitales. Centrado en desarrollar
-            aplicaciones responsivas y desarrollo web mientras aprendo mas
-            tecnologias de backend.
+          <h1 className='py-2 text-gray-700'>Desarrolloador web</h1>
+          <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
+            Sentrado en crear aplicaciones web respondibas para todo tipo de 
+            negocios.
           </p>
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <FaLinkedin />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <FaGithub />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <AiOutlineMail />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <BsFillPersonLinesFill />
-            </div>
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <a
+              href='https://www.linkedin.com/in/clint-briley-50056920a/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaLinkedinIn />
+              </div>
+            </a>
+            <a
+              href='https://github.com/fireclint'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaGithub />
+              </div>
+            </a>
+            <Link href='/#contact'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineMail />
+              </div>
+            </Link>
+            <Link href='/resume'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <BsFillPersonLinesFill />
+              </div>
+            </Link>
           </div>
         </div>
       </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,36 +1,34 @@
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import {
-  AiOutlineCloseCircle,
-  AiOutlineMail,
-  AiOutlineMenu,
-} from "react-icons/ai";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-import { useRouter } from "next/router";
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+// import { useRouter } from 'next/router';
+import NavLogo from '../public/assets/logo.png'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const [shadow, setShadow] = useState(false);
-  const [navBg, setNavBg] = useState("#ecf0f3");
-  const [linkColor, setLinkColor] = useState("#1f2937");
-  const router = useRouter();
+  const [navBg, setNavBg] = useState('#ecf0f3');
+  const [linkColor, setLinkColor] = useState('#1f2937');
+  // const [position, setPosition] = useState('fixed')
+  // const router = useRouter();
 
-  useEffect(()=>{
-    if (
-    router.asPath === '/property' ||
-    router.asPath === '/netflix' ||
-    router.asPath === '/crypto' ||
-    router.asPath === 'twitch'
-    ){
-      setNavBg('transparent')
-      setLinkColor('#ecf0f3')
-    }else{
-      setNavBg('#ecf0f3')
-      setLinkColor('#1f2937')
-    }
-  },[router])
+  // useEffect(() => {
+  //   if (
+  //     router.asPath === '/property' ||
+  //     router.asPath === '/crypto' ||
+  //     router.asPath === '/netflix' ||
+  //     router.asPath === '/twitch'
+  //   ) {
+  //     setNavBg('transparent');
+  //     setLinkColor('#ecf0f3');
+  //   } else {
+  //     setNavBg('#ecf0f3');
+  //     setLinkColor('#1f2937');
+  //   }
+  // }, [router]);
 
   const handleNav = () => {
     setNav(!nav);
@@ -44,141 +42,172 @@ const Navbar = () => {
         setShadow(false);
       }
     };
-    window.addEventListener("scroll", handleShadow);
+    window.addEventListener('scroll', handleShadow);
   }, []);
 
   return (
     <div
-    style={{backgroundcolor: `${navBg}`}}
+      style={{ backgroundColor: `${navBg}` }}
       className={
         shadow
-          ? "fixed w-full h-20 shadow-xl z-[100]"
-          : "fixed w-full h-20 z-[100]"
+          ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300'
+          : 'fixed w-full h-20 z-[100]'
       }
     >
-      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Link href="/">
-          <Image
-            src="/../public/assets/logo.png"
-            alt="/"
-            width="50"
-            height="50"
-          />
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Link href='/'>
+          <a>
+            <Image
+              src={NavLogo}
+              alt='/'
+              width='125'
+              height='50'
+              className='cursor-pointer'
+            />
+          </a>
         </Link>
         <div>
-          <ul style={{color: `${linkColor}`}} className="hidden md:flex">
-            <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
-            </Link>
-            <Link href="/#about">
-              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
-            </Link>
-            <Link href="/#skills">
-              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
-            </Link>
-            <Link href="/#projects">
-              <li className="ml-10 text-sm uppercase hover:border-b">
-                Projects
-              </li>
-            </Link>
-            <Link href="/#contact">
-              <li className="ml-10 text-sm uppercase hover:border-b">
-                Contact
-              </li>
-            </Link>
+          <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/'>Home</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#about'>About</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#skills'>Skills</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#projects'>Projects</Link>
+            </li>
+           
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/#contact'>Contact</Link>
+            </li>
           </ul>
-          <div onClick={handleNav} className="md:hidden">
+
+
+          {/* Hamburger Icon */}
+          <div
+            style={{ color: `${linkColor}` }}
+            onClick={handleNav}
+            className='md:hidden'
+          >
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
 
+      {/* Mobile Menu */}
+      {/* Overlay */}
       <div
         className={
-          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+          nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''
         }
       >
+        {/* Side Drawer Menu */}
         <div
           className={
             nav
-              ? "fixed  left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
-              : "fixed  left-[-100%] top-0  p-10 ease-in duration-500"
+              ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
           }
         >
           <div>
-            <div className="flex w-full items-center justify-between">
-              <Link href="/">
-                <Image
-                  src="/../public/assets/logo.png"
-                  alt="/"
-                  width="45"
-                  height="45"
-                />
+            <div className='flex w-full items-center justify-between'>
+              <Link href='/'>
+                <a>
+                  <Image
+                    src={NavLogo}
+                    width='87'
+                    height='35'
+                    alt='/'
+                  />
+                </a>
               </Link>
               <div
                 onClick={handleNav}
-                className=" rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+                className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'
               >
-                <AiOutlineCloseCircle />
+                <AiOutlineClose />
               </div>
             </div>
-            <div className="border-b border-gray-300 my-4">
-              <p className="w-[85%] md:w-[90%] py-4">
-                Construllamos algo legendario juntos
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w-[90%] py-4'>
+                Let&#39;s build something legendary together
               </p>
             </div>
           </div>
-          <div className="py-4 flex flex-col">
-            <ul className="uppercase">
-              <Link href="/">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <Link href='/'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
                   Home
                 </li>
               </Link>
-              <Link href="/#about">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
+              <Link href='/#about'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
                   About
                 </li>
               </Link>
-              <Link href="/#skills">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
+              <Link href='/#skills'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
                   Skills
                 </li>
               </Link>
-              <Link href="/#projects">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
+              <Link href='/#projects'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
                   Projects
                 </li>
               </Link>
-              <Link href="/#contact">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
+              
+              <Link href='/#contact'>
+                <li onClick={() => setNav(false)} className='py-4 text-sm'>
                   Contact
                 </li>
               </Link>
             </ul>
-            <div className="pt-40">
-              <p className="uppercase tracking-widest text-[#5651e5]">
-                Contactanos
+            <div className='pt-40'>
+              <p className='uppercase tracking-widest text-[#5651e5]'>
+                Let&#39;s Connect
               </p>
-              <div className="flex items-center justify-betweenmy-y w-full sm:w-[80%]">
-                <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-105 ease-in duration-300">
-                  <FaLinkedin />
-                </div>
-              </div>
-              <div className="flex items-center justify-betweenmy-y w-full sm:w-[80%]">
-                <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-105 ease-in duration-300">
-                  <FaGithub />
-                </div>
-              </div>
-              <div className="flex items-center justify-betweenmy-y w-full sm:w-[80%]">
-                <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-105 ease-in duration-300">
-                  <AiOutlineMail />
-                </div>
-              </div>
-              <div className="flex items-center justify-betweenmy-y w-full sm:w-[80%]">
-                <div className="rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-105 ease-in duration-300">
-                  <BsFillPersonLinesFill />
-                </div>
+              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                <a
+                  href='https://github.com/luis-more'
+                  target='_blank'
+                  rel='more'
+                >
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaLinkedinIn />
+                  </div>
+                </a>
+                <a
+                  href='https://github.com/luis-more'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                </a>
+
+                
+                <Link href='https://mail.google.com/mail/u/0/?tab=rm#inbox'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <AiOutlineMail />
+                  </div>
+                </Link>
+                <Link href='/#contact'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <BsFillPersonLinesFill />
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
